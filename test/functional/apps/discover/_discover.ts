a8fa49e03133ebec7305273fba85ef7e3df330c0
Tre' Seymour <wayne.seymour@elastic.co>
2021-02-03 16:19:34 -0700

    [QA] Create Saved Objects FTR Svc
    Add svc,
    integrate into one test,
    add exported saved objects,
    add empty kibana call,
    and update ftr schema...add data directory for the new svc.

--- test/functional/apps/discover/_discover.ts
+++ test/functional/apps/discover/_discover.ts
@@ -19,7 +19,9 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const queryBar = getService('queryBar');
   const inspector = getService('inspector');
   const elasticChart = getService('elasticChart');
+  const savedObjects = getService('savedObjects');
   const PageObjects = getPageObjects(['common', 'discover', 'header', 'timePicker']);
+
   const defaultSettings = {
     defaultIndex: 'logstash-*',
   };
@@ -27,7 +29,9 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   describe('discover test', function describeIndexTests() {
     before(async function () {
       log.debug('load kibana index with default index pattern');
-      await esArchiver.load('discover');
+
+      await esArchiver.load('empty_kibana');
+      await savedObjects.import('discover');
 
       // and load a set of makelogs data
       await esArchiver.loadIfNeeded('logstash_functional');
