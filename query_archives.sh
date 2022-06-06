2c65a53fad9778e527736e62037cb9d245e33d71
Tre' Seymour <wayne.seymour@elastic.co>
2022-06-06 14:45:46 +0100

    thinking

---
+++ query_archives.sh
@@ -0,0 +1,20 @@
+
+collect() {
+  xpack=$(fgrep -ri '"index": ".kibana' x-pack/test --include=mappings.json | awk '{print $1}' | sed -e 's|:||')
+  oss=$(fgrep -ri '"index": ".kibana' test --include=mappings.json | awk '{print $1}' | sed -e 's|:||')
+}
+
+standardList=("url" "index-pattern" "action" "query" "alert" "graph-workspace" "tag" "visualization" "canvas-element" "canvas-workpad" "dashboard" "search" "lens" "map" "cases" "uptime-dynamic-settings" "osquery-saved-query" "osquery-pack" "infrastructure-ui-source" "metrics-explorer-view" "inventory-view" "infrastructure-monitoring-log-view" "apm-indices")
+
+for x in ${standardList[@]}; do
+  echo $x
+done
+
+
+#for xpackArchive in $(fgrep -ri '"index": ".kibana' x-pack/test --include=mappings.json | awk '{print $1}' | sed -e 's|:||'); do
+#  echo "### xpackArchive: ${xpackArchive}"
+#done
+
+typesPerArchive() {
+  
+}
