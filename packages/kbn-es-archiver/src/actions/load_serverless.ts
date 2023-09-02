2ffdf9e7e1950c5745fe14d27f4b45cc2784826a
Tre' Seymour <wayne.seymour@elastic.co>
2023-09-02 03:51:22 +0100

    got the names resolved

--- packages/kbn-es-archiver/src/actions/load_serverless.ts
+++ packages/kbn-es-archiver/src/actions/load_serverless.ts
@@ -1,15 +1,20 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one * or more contributor license agreements. Licensed under the Elastic License * 2.0 and the Server Side Public License, v 1; you may not use this file except * in compliance with, at your election, the Elastic License 2.0 or the Server * Side Public License, v 1. */
 
-import { from, fromEventPattern } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {from, fromEventPattern} from 'rxjs';
+import {map, concat} from 'rxjs/operators';
+import {REPO_ROOT} from '@kbn/repo-info';
 import * as zlib from 'zlib';
-import { readdir } from 'fs/promises';
+import {readdir} from 'fs/promises';
 import * as fs from 'fs';
 import oboe from 'oboe';
-import { pipe } from 'fp-ts/function';
-import { resolve } from 'path';
-import { prioritizeMappings } from '../lib';
+import {pipe} from 'fp-ts/function';
+import {resolve} from 'path';
+import {toError} from 'fp-ts/Either';
+// import * as TE from 'fp-ts/TaskEither';
+import {prioritizeMappings} from '../lib';
+import {writeFileSync} from 'fs';
+
 
 type PredicateFunction = (a: string) => boolean;
 type PathLikeOrString = fs.PathLike | string;
@@ -18,10 +23,6 @@ const doesNotStartWithADot: PredicateFunction = (x) => !x.startsWith('.');
 const readDirectory = (predicate: PredicateFunction) => {
   return async (path: string) => (await readdir(path)).filter(predicate);
 };
-
-const toStr = (x: BufferSource) => `${x}`;
-const decompressionObservable = (x: PathLikeOrString) =>
-  from(fs.createReadStream(x).pipe(zlib.createGunzip())).pipe(map(toStr));
 
 // const subscribeToDecompressionStream = (archivePath: PathLikeOrString) => {
 //   decompressionObservable(archivePath).subscribe({
@@ -46,6 +47,29 @@ const jsonStanzaObservable = (x) => fromEventPattern(jsonStanza$(x));
 //   });
 // };
 
+const errFilePath = () => resolve(REPO_ROOT, 'esarch_failed_load_action_archives.txt');
+const handleErrToFile = (archivePath) => (reason) => {
+  const {code, stack, message} = reason;
+
+  const caught = {
+    code,
+    stack,
+    message,
+    archiveThatFailed: archivePath,
+  };
+  const failedMsg = `${JSON.stringify(caught, null, 2)}`;
+
+  try {
+    throw new Error(`${reason}`);
+  } catch (err) {
+    console.error(failedMsg);
+    writeFileSync(errFilePath(), `${failedMsg},\n`, {flag: 'a', encoding: 'utf8'});
+  }
+
+  return toError(reason);
+}
+
+
 type ArchivePathEntry = string;
 
 const resolveEntry = (archivePath: PathLikeOrString) => (x: ArchivePathEntry) =>
@@ -58,6 +82,23 @@ const resolveAndPrioritizeArchiveEntriesObservable =
   (archivePath: PathLikeOrString) => (xs: ArchivePathEntry[]) =>
     from(pipe(prioritizeMappings)(xs)).pipe(map(resolveEntry(archivePath)));
 
+// const archiveEntries = async (archivePath: PathLikeOrString) => await pipe(
+//   TE.tryCatch(
+//     async () => await mappingsAndArchiveFileNames(archivePath),
+//     (reason: any) => toError(reason)
+//   ),
+//   TE.map((x) => {
+//     console.log(`\nλjs task either x: \n\t${x}`);
+//     return x;
+//   }),
+//   TE.getOrElse((e) => handleErrToFile(archivePath)(e))
+// )();
+
+const toStr = (x: BufferSource) => `${x}`;
+const decompressionObservable = (x: PathLikeOrString) =>
+  from(fs.createReadStream(x).pipe(zlib.createGunzip())).pipe(map(toStr));
+
+
 export const begin = async (archivePath: PathLikeOrString): Promise<void> => {
   // const archiveFilePath =
   //   '/Users/trezworkbox/dev/scratches/src/js/streams/native-nodejs-streams/gunzip/someotherfile.txt.gz';
@@ -67,6 +108,8 @@ export const begin = async (archivePath: PathLikeOrString): Promise<void> => {
   // archivePath =
   //   '/Users/trezworkbox/dev/scratches/src/js/streams/native-nodejs-streams/gunzip/archive-path';
 
+  // const xs = await archiveEntries(archivePath);
+
   resolveAndPrioritizeArchiveEntriesObservable(archivePath)(
     await mappingsAndArchiveFileNames(archivePath)
   ).subscribe({
@@ -75,9 +118,27 @@ export const begin = async (archivePath: PathLikeOrString): Promise<void> => {
     complete: () => console.log('the end'),
   });
 
-  // concat(decompressionObservable(archiveFilePath), jsonStanzaObservable(archivePath)).subscribe({
 
-  // concat(decompressionObservable(archivePath), jsonStanzaObservable(archivePath)).subscribe({
+  // concat(
+  //   resolveAndPrioritizeArchiveEntriesObservable(archivePath)(
+  //     await mappingsAndArchiveFileNames(archivePath)
+  //   ),
+  //   // decompressionObservable(archivePath),
+  //   jsonStanzaObservable(archivePath)
+  // ).subscribe({
+  //   next: (x) => console.log('\nλjs next, x:', x),
+  //   error: (err) => console.log('error:', err),
+  //   complete: () => console.log('the end'),
+  // });
+
+
+  // concat(
+  //   resolveAndPrioritizeArchiveEntriesObservable(archivePath)(
+  //     await mappingsAndArchiveFileNames(archivePath)
+  //   ),
+  //   // decompressionObservable(archivePath),
+  //   // jsonStanzaObservable(archivePath)
+  // ).subscribe({
   //   next: (x) => console.log('\nλjs next, x:', x),
   //   error: (err) => console.log('error:', err),
   //   complete: () => console.log('the end'),
