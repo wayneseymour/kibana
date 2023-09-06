5d47c2f7bd45fa02ddb7fbddd22f362665f7c7a7
Tre' Seymour <wayne.seymour@elastic.co>
2023-09-06 07:28:59 +0100

    pipeline over pipe for better err handling

--- packages/kbn-es-archiver/src/actions/load_serverless.ts
+++ packages/kbn-es-archiver/src/actions/load_serverless.ts
@@ -4,17 +4,26 @@
 import * as zlib from 'zlib';
 import * as fs from 'fs';
 import oboe from 'oboe';
+import { pipeline, PassThrough } from 'node:stream';
 import { resolve } from 'path';
 import { PathLikeOrString, archiveEntries, ArchivePathEntry } from './load_utils';
 
-const toStr = (x: BufferSource) => `${x}`;
-const noop = () => {};
 const resolveEntry = (archivePath: PathLikeOrString) => (x: ArchivePathEntry) =>
   resolve(archivePath as string, x);
-const readAndUnzip$ = (x) => oboe(fs.createReadStream(x).pipe(zlib.createGunzip()));
+const gzip = true;
+const readAndUnzip$ = (x) =>
+  oboe(
+    pipeline(fs.createReadStream(x), gzip ? zlib.createGunzip() : new PassThrough(), (err) => {
+      if (err) {
+        console.error('Pipeline failed.', err);
+      } else {
+        console.log('Pipeline succeeded.');
+      }
+    })
+  );
 const jsonStanza$ = (x) =>
-  readAndUnzip$(x).on('done', function theUglyCallback(obj) {
-    console.log(`\nλjs obj: \n${JSON.stringify(obj, null, 2)}`);
+  readAndUnzip$(x).on('done', function theUglyCallback(jsonRecord) {
+    console.log(`\nλjs jsonRecord: \n${JSON.stringify(jsonRecord, null, 2)}`);
     // process.exit(666); // Trez Exit Expression
   });
 export const begin = async (pathToArchiveDirectory: PathLikeOrString) => {
