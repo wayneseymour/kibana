64f99d0f900c9196430a37cc4271e5cb210ae391
Tre' Seymour <wayne.seymour@elastic.co>
2023-09-07 15:15:32 +0100

    more straight piping. Now I've to integrate the index create into the straight pipe. Tbh, I nearly forgot about this.

--- packages/kbn-es-archiver/src/actions/load.ts
+++ packages/kbn-es-archiver/src/actions/load.ts
@@ -6,31 +6,31 @@
  * Side Public License, v 1.
  */
 
-import { resolve, relative } from 'path';
-import { createReadStream } from 'fs';
-import { PassThrough, Readable } from 'stream';
+import { relative } from 'path';
 import { ToolingLog } from '@kbn/tooling-log';
 import { REPO_ROOT } from '@kbn/repo-info';
 import type { KbnClient } from '@kbn/test';
 import type { Client } from '@elastic/elasticsearch';
-import { concatStreamProviders } from '@kbn/utils';
+import { createPromiseFromStreams } from '@kbn/utils';
 
+import { MAIN_SAVED_OBJECT_INDEX } from '@kbn/core-saved-objects-server';
+import { pipe } from 'fp-ts/lib/function';
 import {
-  isGzip,
+  atLeastOne,
+  readDir$AndCreateStanzasViaHandJamming$,
+  freshenUp,
+  hasDotKibanaPrefix,
+  indexingOccurred,
+} from './load_utils';
+import {
   createStats,
-  createParseArchiveStreams,
-
-  // createIndexDocRecordsStream,
+  prioritizeMappings,
+  readDirectory as mappingAndDataFile,
+  createCreateIndexStream as oldCreateIndexOrDataStream$,
+  migrateSavedObjectIndices,
+  createDefaultSpace,
+  createIndexDocRecordsStreamSvrLess,
 } from '../lib';
-import { straightPipe } from './load_serverless';
-
-// pipe a series of streams into each other so that data and errors
-// flow from the first stream to the last. Errors from the last stream
-// are not listened for
-const pipeline = (...streams: Readable[]) =>
-  streams.reduce((source, dest) =>
-    source.once('error', (error) => dest.destroy(error)).pipe(dest as any)
-  );
 
 export async function loadAction({
   inputDir,
@@ -49,51 +49,38 @@ export async function loadAction({
   log: ToolingLog;
   kbnClient: KbnClient;
 }) {
-  const name = relative(REPO_ROOT, inputDir);
-  const stats = createStats(name, log);
-  await straightPipe(name);
-  // process.exit(666); // Trez Exit Expression
-  // await createPromiseFromStreams([
-  //   // This used to be  const recordStream = concatStreamProviders()...
-  //   bothFiles$(inputDir, prioritizeMappings(await readDirectory(inputDir))),
-  //   createCreateIndexStream({ client, stats, skipExisting, docsOnly, log }),
-  //   createIndexDocRecordsStreamWithoutProgess(client, stats, useCreate),
-  //   // createIndexDocRecordsStream(client, stats, progress, useCreate),
-  //   // createIndexDocRecordsStreamSvrLess(client, stats, useCreate),
-  // ]);
-  //
-  // const result = stats.toJSON();
-  //
-  // const indicesWithDocs: string[] = [];
-  // for (const [index, { docs }] of Object.entries(result))
-  //   if (indexingOccurred(docs)) indicesWithDocs.push(index);
-  //
-  // await freshenUp(client, indicesWithDocs);
-  //
-  // // If we affected saved objects indices, we need to ensure they are migrated...
-  // if (atLeastOne(hasDotKibanaPrefix(MAIN_SAVED_OBJECT_INDEX))(result)) {
-  //   await migrateSavedObjectIndices(kbnClient);
-  //   // WARNING affected by #104081. Assumes 'spaces' saved objects are stored in MAIN_SAVED_OBJECT_INDEX
-  //   if ((await kbnClient.plugins.getEnabledIds()).includes('spaces'))
-  //     await createDefaultSpace({ client, index: MAIN_SAVED_OBJECT_INDEX });
-  // }
-  // return result;
-  //
-  // // a single stream that emits records from all archive files, in
-  // // order, so that createIndexStream can track the state of indexes
-  // // across archives and properly skip docs from existing indexes
-  function bothFiles$(
-    archiveDirectory: string,
-    maybeMappingsAndDocsFileNamesFromArchive: string[]
-  ): PassThrough {
-    return concatStreamProviders(
-      maybeMappingsAndDocsFileNamesFromArchive.map((filename: string) => () => {
-        return pipeline(
-          createReadStream(resolve(archiveDirectory, filename)),
-          ...createParseArchiveStreams({ gzip: isGzip(filename) })
-        );
-      }),
-      { objectMode: true }
-    );
+  const relFromRoot = relative.bind(null, REPO_ROOT);
+  const relativeArchivePath = relFromRoot(inputDir);
+
+  const stats = createStats(relativeArchivePath, log);
+  await createPromiseFromStreams([
+    pipe(
+      await mappingAndDataFile(inputDir),
+      prioritizeMappings,
+      readDir$AndCreateStanzasViaHandJamming$(inputDir)
+    ),
+    oldCreateIndexOrDataStream$({ client, stats, skipExisting, docsOnly, log }),
+    createIndexDocRecordsStreamSvrLess(client, stats, useCreate),
+  ]);
+  // await straightPipe(relativeArchivePath);
+  /*
+   The two expressions above could be a promise.all or TaskEither.sequence!
+   */
+
+  const result = stats.toJSON();
+
+  const indicesWithDocs: string[] = [];
+  for (const [index, { docs }] of Object.entries(result))
+    if (indexingOccurred(docs)) indicesWithDocs.push(index);
+
+  await freshenUp(client, indicesWithDocs);
+
+  // If we affected saved objects indices, we need to ensure they are migrated...
+  if (atLeastOne(hasDotKibanaPrefix(MAIN_SAVED_OBJECT_INDEX))(result)) {
+    await migrateSavedObjectIndices(kbnClient);
+    // WARNING affected by #104081. Assumes 'spaces' saved objects are stored in MAIN_SAVED_OBJECT_INDEX
+    if ((await kbnClient.plugins.getEnabledIds()).includes('spaces'))
+      await createDefaultSpace({ client, index: MAIN_SAVED_OBJECT_INDEX });
   }
+  return result;
 }
---
+++ packages/kbn-es-archiver/src/actions/straight_pipe.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one * or more contributor license agreements. Licensed under the Elastic License * 2.0 and the Server Side Public License, v 1; you may not use this file except * in compliance with, at your election, the Elastic License 2.0 or the Server * Side Public License, v 1. */
+
+import { pipe } from 'fp-ts/lib/function';
+import {
+  PathLikeOrString,
+  archiveEntries,
+  prepareForStanzation,
+  jsonStanza$Subscription,
+  subscribe,
+} from './load_utils';
+
+export const straightPipe = async (pathToArchiveDirectory: PathLikeOrString): Promise<void> => {
+  (await archiveEntries(pathToArchiveDirectory))
+    .map(prepareForStanzation(pathToArchiveDirectory)) // This internal iteration is only handling 2 strings
+    .map(pipe(jsonStanza$Subscription, subscribe));
+};
+
+export const straightPipeWithIndexCreation = async (
+  pathToArchiveDirectory: PathLikeOrString
+): Promise<void> => {
+  (await archiveEntries(pathToArchiveDirectory))
+    .map(prepareForStanzation(pathToArchiveDirectory)) // This internal iteration is only handling 2 strings
+    .map(pipe(jsonStanza$Subscription, subscribe));
+};
