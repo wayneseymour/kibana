5980e7519b3a7c32c8409c52f186b2c60ada459a
Tre' Seymour <wayne.seymour@elastic.co>
2023-09-08 16:45:14 +0100

    are we there yet?

--- packages/kbn-es-archiver/src/actions/straight_pipe.ts
+++ packages/kbn-es-archiver/src/actions/straight_pipe.ts
@@ -2,7 +2,7 @@
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one * or more contributor license agreements. Licensed under the Elastic License * 2.0 and the Server Side Public License, v 1; you may not use this file except * in compliance with, at your election, the Elastic License 2.0 or the Server * Side Public License, v 1. */
 
 import { PathLikeOrString, resolveAndAnnotateForDecompression, Annotated } from './load_utils';
-import { archiveEntries } from './straight_pipe_utils';
+import {allWrapper$, archiveEntries, handleStreamToFileWithLimit } from './straight_pipe_utils';
 
 // export const straightPipe = async (pathToArchiveDirectory: PathLikeOrString): Promise<void> => {
 //   (await archiveEntries(pathToArchiveDirectory))
@@ -19,8 +19,7 @@ export const straightPipeAll =
         const { entryAbsPath, needsDecompression } = x;
         console.log(`\nλjs entryAbsPath: \n\t${entryAbsPath}`);
 
-        const i: number = 0;
-        allWrapper$(entryAbsPath)(needsDecompression)(handleStreamToFileWithLimit(i))(indexingArgs);
+        allWrapper$(entryAbsPath)(needsDecompression)(handleStreamToFileWithLimit(0))(indexingArgs);
       });
     // .map(pipe(jsonStanza$Subscription, subscribe));
   };
