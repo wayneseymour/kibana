f4083180844c9f6b5cfb98ee3c426240582f0d09
Tre' Seymour <wayne.seymour@elastic.co>
2023-09-08 16:43:06 +0100

    cleanup?

--- packages/kbn-es-archiver/src/actions/straight_pipe.ts
+++ packages/kbn-es-archiver/src/actions/straight_pipe.ts
@@ -1,21 +1,14 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one * or more contributor license agreements. Licensed under the Elastic License * 2.0 and the Server Side Public License, v 1; you may not use this file except * in compliance with, at your election, the Elastic License 2.0 or the Server * Side Public License, v 1. */
 
-import { pipe } from 'fp-ts/lib/function';
-import {
-  PathLikeOrString,
-  resolveAndAnnotateForDecompression,
-  jsonStanza$Subscription,
-  subscribe,
-  Annotated,
-} from './load_utils';
-import { allWrapper$, archiveEntries, handleStreamToFileWithLimit } from './straight_pipe_utils';
+import { PathLikeOrString, resolveAndAnnotateForDecompression, Annotated } from './load_utils';
+import { archiveEntries } from './straight_pipe_utils';
 
-export const straightPipe = async (pathToArchiveDirectory: PathLikeOrString): Promise<void> => {
-  (await archiveEntries(pathToArchiveDirectory))
-    .map(resolveAndAnnotateForDecompression(pathToArchiveDirectory))
-    .map(pipe(jsonStanza$Subscription, subscribe));
-};
+// export const straightPipe = async (pathToArchiveDirectory: PathLikeOrString): Promise<void> => {
+//   (await archiveEntries(pathToArchiveDirectory))
+//     .map(resolveAndAnnotateForDecompression(pathToArchiveDirectory))
+//     .map(pipe(jsonStanza$Subscription, subscribe));
+// };
 
 export const straightPipeAll =
   (pathToArchiveDirectory: PathLikeOrString) =>
